ac75a44be037fc08100d43764d3d77c7a6cdc1ce
Patrice976 <catinatp@gmail.com>
2025-04-16 11:20:20 +0200

    feat(landingPage): création de la landing page, problématique avec la div mask qui doit contenir image, changmement de plan autour du design de la landing page

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <img src="/src/assets/img/logo.png" alt="logo MainMaker" />
+    <nav>
+      <ul>
+        <li><a href="#">Personnages</a></li>
+        <li><a href="#">Stat</a></li>
+        <li><a href="#">Autre jeux</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <button>Connexion</button>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  background: linear-gradient(to bottom, $main-color, $lightbg-color);
+  display: flex;
+  padding-inline: 3rem;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  nav {
+    ul {
+      display: flex;
+      gap: 2rem;
+      align-items: center;
+      width: 100%;
+    }
+  }
+  img {
+    width: auto;
+    height: 100%;
+  }
+}
+</style>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import MainHeader from '../components/MainHeader.vue'
+</script>
+
+<template>
+  <MainHeader />
+  <main>
+    <div class="main-content">
+      <div class="title">
+        <h2>Bienvenue sur</h2>
+        <h1>MainMaker</h1>
+      </div>
+      <p>Le site qui t'aideras à trouver ton main et à briller dans tes jeux préférés.</p>
+      <button>Choisis tes persos préféré</button>
+      <button>Notes tes performances</button>
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  height: 100dvh;
+  width: auto;
+  background-color: $lightbg-color;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+}
+
+.img-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70%;
+  width: 40%;
+  overflow: hidden;
+  background-image: url('/src/assets/img/SmiteLandingImg.webp');
+  background-size: cover;
+  background-color: black;
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
+  img {
+    height: 50%;
+    width: auto;
+    object-fit: cover;
+  }
+}
+</style>
