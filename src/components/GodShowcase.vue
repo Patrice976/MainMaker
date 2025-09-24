ef5378d6ebf41a59d044d24f41059c01f1bb7cec
Patrice976 <catinatp@gmail.com>
2025-09-24 14:29:16 +0200

    feat(GodShowcase): ajouter animation traits doré autour du logo au hover

--- src/components/GodShowcase.vue
+++ src/components/GodShowcase.vue
@@ -8,16 +8,18 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
+  <div class="card">
     <img class="god_img_background" :src="props.god.godImg" alt="" />
     <h1 class="god_name">{{ props.god.name }}</h1>
-    <img class="pantheon_logo" :src="props.god.pantheonImg" alt="" />
+    <div class="banner_logo">
+      <img class="pantheon_logo" :src="props.god.pantheonImg" alt="" />
+    </div>
     <h2 class="god_title">{{ props.god.title }}</h2>
   </div>
 </template>
 
 <style scoped lang="scss">
-div {
+.card {
   margin: 5rem;
   display: flex;
   height: 300px;
@@ -27,17 +29,55 @@ div {
   background-image: url(props.god.godImg);
 }
 
+.card:hover {
+  cursor: pointer;
+  transform: scale(1.05);
+  filter: brightness(1.5);
+  transition: transform 0.2s ease-in-out;
+}
+
+.banner_logo {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.banner_logo::before {
+  content: '';
+  position: absolute;
+  left: 1rem;
+  top: 1rem;
+  height: 1px;
+  width: 0%;
+  background: linear-gradient(to right, #58341f, #ffcc73);
+}
+
+.banner_logo::after {
+  content: '';
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  height: 1px;
+  width: 0%;
+  background: linear-gradient(to right, #ffcc73, #58341f);
+}
+.card:hover .banner_logo::after,
+.card:hover .banner_logo::before {
+  width: 30%;
+  transition: width 0.5s ease-in-out;
+}
+
 h1 {
   margin: 0.5rem;
   text-align: center;
-  font-size: 2.5cap;
-  color: $contrast-txt-color;
+  font-size: 2rem;
+  color: $lightbg-color;
   text-shadow: 2px 2px 4px #000000;
 }
 h2 {
   margin: 0.5rem;
   text-align: center;
-  color: $contrast-txt-color;
+  color: $lightbg-color;
   text-shadow: 2px 2px 4px #000000;
   font-size: 1rem;
 }
@@ -54,5 +94,8 @@ h2 {
   border-radius: 20px;
   filter: brightness(0.5);
   z-index: -1;
+}
+.hide {
+  display: none;
 }
 </style>
